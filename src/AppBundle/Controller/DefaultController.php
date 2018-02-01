6ff925e524a8a32a3b0a7c5623a5dbc9a457b211
bladess <bladess@hotmail.fr>
2018-02-01 15:52:03 +0100

    acceuil admin avec liens

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,12 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    /**
+     * @Route("admin", name="index_admin")
+     */
+    public function adminAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/indexAdmin.html.twig');
+    }
 }
