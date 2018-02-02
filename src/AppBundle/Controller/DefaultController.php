61b85b996a347006c58934b851d6f34ed9a842b7
bladess <bladess@hotmail.fr>
2018-02-02 11:55:09 +0100

    affichage listing annonces avec formulaire de recherche

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use AppBundle\Entity\TypeAnnonce;
 class DefaultController extends Controller
 {
     /**
@@ -23,19 +25,60 @@ class DefaultController extends Controller
      */
     public function adminAction()
     {
-        
         return $this->render('default/indexAdmin.html.twig');
     }
     /**
      * @Route("/annonces", name="liste_annonces")
      */
-    public function listAction()
+    public function listAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
+        $types = $this->getDoctrine()->getRepository('AppBundle:TypeAnnonce')->findAll();
+        $listTypes = ['Tout types' => 0];
+        foreach($types as $key => $value){
+            $listTypes += [$value->getIntitule() => $value->getId()];
+        }    
+        // dump($listTypes);
+        // die();
 
-        $annonces = $em->getRepository('AppBundle:Annonce')->findAll();
 
-        return $this->render('default/annonces.html.twig', ['annonces' => $annonces]);
+        $form = $this->createFormBuilder()
+        ->add('search', SearchType::class, array('required'   => false,'label' => ' ','attr' => array(
+                'placeholder' => 'rechercher une annonce',
+            )))->add('typeannonce', ChoiceType::class,array('choices'=> $listTypes,'label'=>' '))
+        ->getForm();
+        $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+            $search = $form->getData()['search'];
+            $typeS = $form->getData()['typeannonce'];
+            dump($typeS);
+            if($typeS === 0){
+                $query = $em->createQuery('SELECT a FROM AppBundle\Entity\Annonce a WHERE a.titre like :p')
+                ->setParameter('p','%'.$search.'%');
+            }
+            else{
+                $query = $em->createQuery('SELECT a FROM AppBundle\Entity\Annonce a JOIN a.typeAnnonce t WHERE a.titre like :p and t.id = :q')
+                ->setParameters(['p'=>'%'.$search.'%', 'q' => $typeS]);
+            }
+            $annonces = $query->getResult();
+        }
+        else{
+            $annonces = $em->getRepository('AppBundle:Annonce')->findAll();
+        }
+
+        return $this->render('default/annonces.html.twig', 
+        ['annonces' => $annonces,
+        'form' => $form->createView()
+        ]);
     }
-
+    /**
+     * @Route("/annonce/{id}", name="liste_annonces")
+     */
+    public function showAction($id){
+        $annonce = $this->getDoctrine()->getRepository('AppBundle:Annonce')->find($id);
+        return $this->render('default/show.html.twig', 
+        ['annonce' => $annonce,
+        ]);
+    }
 }
